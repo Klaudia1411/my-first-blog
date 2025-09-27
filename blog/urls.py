d72db314300b61c3ae44909bf93a6448fdc92a55
Tvoje meno <ty@example.com>
2025-09-27 16:36:09 +0200

    verzia2

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from .forms import PostForm
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+]    
