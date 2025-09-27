a38a15812ad204b20ab9c68b9623677f2ae7eefd
Tvoje meno <ty@example.com>
2025-09-27 12:14:21 +0200

    My Django Girls app, first commit

---
+++ text.py
@@ -0,0 +1,25 @@
+# vypocet = 2+3
+# print(vypocet)
+# if 3>2:
+#     print('spravne')
+# elif 3==3
+#     print('spravne')
+# else:
+#     print('nespravne')
+
+# def fun(premenna, druha):
+#     print(premenna)
+#     print(druha)
+
+# fun('ahoj','cau')
+
+# def meno(meno):
+#     print('ahoj' + meno)
+# meno('Neznamy')
+
+dievcata = ['Ema', 'Klaudia', 'Silvia', 'Ola']
+
+for meno in dievcata:
+    print(meno)
+
+
